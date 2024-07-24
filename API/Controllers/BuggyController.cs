84e59761e773fc6a24a135f6503c57155204cf68
Patrick Melby <patrick.melby@gmail.com>
2024-07-24 12:30:08 -0700

    End of section 7

---
+++ API/Controllers/BuggyController.cs
@@ -0,0 +1,38 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+public class BuggyController(DataContext context): BaseApiController
+{
+    [Authorize]
+    [HttpGet("auth")]
+    public ActionResult<string> GetAuth()
+    {
+        return "secret text";
+    }
+
+    [HttpGet("not-found")]
+    public ActionResult<AppUser> GetNotFound()
+    {
+        var thing = context.Users.Find(-1);
+
+        if(thing is null) return NotFound();
+
+        return thing;
+    }
+
+    [HttpGet("server-error")]
+    public ActionResult<AppUser> GetServerError()
+    {
+        throw new ApplicationException("Some kind of 500 error");
+    }
+
+    [HttpGet("bad-request")]
+    public ActionResult<AppUser> GetBadRequest()
+    {
+        return BadRequest("Not good...");
+    }
+}
--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -3,14 +3,15 @@ import { provideRouter } from '@angular/router';
 import { provideAnimations } from '@angular/platform-browser/animations';
 
 import { routes } from './app.routes';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { provideToastr } from 'ngx-toastr';
+import { errorInterceptor } from './_interceptor/error.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }), 
     provideRouter(routes),
-    provideHttpClient(),
+    provideHttpClient(withInterceptors([errorInterceptor])),
     provideAnimations(),
     provideToastr({
       positionClass: 'toast-bottom-right'
